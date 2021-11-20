1816913a2bc335d00d30d0ebc5122d0fbe25336f
Armandas <armandas0707@gmail.com>
2021-11-20 19:21:20 +0200

    fixed margin top for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import pogchamp from '../components/assets/pogchamp.png';
 
 const Home = () => {
     return (
-        <div className="text-white text-5xl md:text-8xl font-bold font-sans text-center mt-52">
+        <div className="text-white text-5xl md:text-8xl font-bold font-sans text-center md:mt-52">
             <h1> FILLQ SUGRĮŽTA </h1><img src={pogchamp} className="object-contain inline" alt="PogChamp"></img> 
             <h2 className="mt-6 text-4xl"> Sekite naujienas: 
                 <AiOutlineFacebook className="cursor-pointer text-6xl inline mb-1 mx-2" onClick={()=> window.open("https://www.facebook.com/ironwolveslt", "_blank")}></AiOutlineFacebook> 
