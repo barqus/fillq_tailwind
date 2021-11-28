438206bf117ac644a8cab7133666d9d8427bbdda
Armandas <armandas0707@gmail.com>
2021-11-28 22:20:25 +0200

    WIP

--- src/components/utils/fetchApi.js
+++ src/components/utils/fetchApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const fetchDataCall = async ({ api }) => {
     const token = "ya29.a0ARrdaM8JiGts1qxlF0c1ktmHY07FT1TJ-Hqen5E6OLCGammK53bbHwNg3Xn6wzCBOOvecR2CvzS7aAdeKpKSGsjNA2huRy4sPIlr_gCe9WkgrSUz5PVRRGNU5SOVcKBxWVvkmPthevtys9pPcjbhXJR7Btv8VvG0-9X_KcPWJlzQa0BRxpwCNguvHzcoQjJqLAoPamp4uqasWra0LWboAjEOA0wWMZoM4A"
     let apiReturn = await axios
-        .get('https://fillq-333518.appspot.com/api/v1/'+api, {headers: { Authorization: `Bearer ${token}` }})
+        .get('https://fillq-333518.appspot.com/api/v1/'+api)
         .then(async function (response) {
             return response;
         })
