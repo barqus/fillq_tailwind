d51ac514f7e11d58bc3f17ea392fbf58e0da65e7
Armandas <armandas0707@gmail.com>
2021-11-28 16:48:57 +0200

    WIP

--- src/components/utils/fetchApi.js
+++ src/components/utils/fetchApi.js
@@ -2,9 +2,7 @@ import axios from 'axios'
 
 const fetchDataCall = async ({ api }) => {
     let apiReturn = await axios
-        .get('http://3.123.229.48:8080/api/v1/'+api, { 	headers: {
-            'Access-Control-Allow-Origin': '*',
-          },crossDomain: true })
+        .get('http://3.123.229.48:8080/api/v1/'+api)
         .then(async function (response) {
             return response;
         })
