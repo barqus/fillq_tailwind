7309d35cc1d36125f409bf8f01c9d1cd3fe1d937
Armandas Barkauskas <52072845+barkisjr@users.noreply.github.com>
2021-12-02 00:18:11 +0200

    WIp

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -29,7 +29,7 @@ const Dropdown = ({isOpen, toggle, userID, setUserID}) => {
     const accountAction = () => {
         if (userID === null) {
             const REACT_APP_TWITCH_CLIENT_ID="by7zl6rwazu7ks1z6sby63bnwq1267"
-            const REACT_APP_TWITCH_REDIRECT_URI = "https://fillq.lt/twitchRedirect"
+            const REACT_APP_TWITCH_REDIRECT_URI = "https://www.fillq.lt/twitchRedirect"
             var twitchLoginURI = `https://id.twitch.tv/oauth2/authorize?client_id=${REACT_APP_TWITCH_CLIENT_ID}&response_type=code&scope=user:read:email&redirect_uri=${REACT_APP_TWITCH_REDIRECT_URI}`
             window.location.href = twitchLoginURI
         }
--- src/components/Sponsors/SponsorsComponent.js
+++ src/components/Sponsors/SponsorsComponent.js
@@ -8,43 +8,42 @@ import Wolt from './PNGS/WOLT.png'
 import CandyPop from './PNGS/CANDY POP.png'
 const SponsorsComponent = () => {
     return (
-        <div className="grid justify-center mb-12  mt-12 text-center text-white text-xl font-bold font-sans">
-            FILLQ INFORMACIJA
+        <div className=" inline-grid justify-center mb-12  mt-12 text-center text-white text-xl font-bold font-sans">
             <div className="bg-gray-900 mt-6 pr-4 pl-4 rounded-xl w-auto">
                 <div className="grid grid-rows-2 p-6 grid-cols-3 gap-10 grid-flow-rows text-left mt-6">
-                    <div>
-                        <img src={TCLogo} alt="logo" />
+                    <div className="flex justify-center items-center">
+                        <img src={TCLogo} alt="logo" className="cursor-pointer" onClick={() => {window.open("https://www.topocentras.lt/", "_blank")}} />
                     </div>
                     <div className="col-span-2">
-                        Nori atsinaujinti savo įrangą ir pakelti savo lygį? Topo Centre rasi viską ko tau reikia. Nuo pilnai sukomplektuoto kompiuterio, iki naujos mechaninės klaviatūros. Hyperlink Topocentras
-                    </div>
-                    <div>
-                        <img src={ViperLogo} alt="logo" />
+                        Nori atsinaujinti savo įrangą ir pakelti savo lygį? Topo Centre rasi viską ko tau reikia. Nuo pilnai sukomplektuoto kompiuterio, iki naujos mechaninės klaviatūros.
+                    </div>  
+                    <div className="flex justify-center items-center">
+                        <img src={ViperLogo}  alt="logo" className="cursor-pointer" onClick={() => {window.open("https://viper.patriotmemory.com/", "_blank")}} />
                     </div>
                     <div className="col-span-2">
-                        Lyderiaujantys SSD, RAM ir periferijos gamintojai. Įsikūrę 1985 metais Amerikoje. Žaidėjams suteikiantys geriausią galios ir kainos santykį rinkoje. (Hyperlink https://viper.patriotmemory.com)
+                        Lyderiaujantys SSD, RAM ir periferijos gamintojai. Įsikūrę 1985 metais Amerikoje. Žaidėjams suteikiantys geriausią galios ir kainos santykį rinkoje.
                     </div>
-                    <div>
-                        <img src={Wolt} className="h-20" alt="logo" />
+                    <div className="flex justify-center items-center">
+                        <img src={Wolt} alt="logo" className="h-20 cursor-pointer" onClick={() => {window.open("https://wolt.com/lt/apply-as-courier?utm_source=Affiliate&utm_medium=banner&utm_campaign=LTU_Vilnius_Web_COURIER_LOCAL_VNO12&utm_content=FILLQ", "_blank")}}/>
                     </div>
+                    {/* https://wolt.com/lt/apply-as-courier?utm_source=Affiliate&utm_medium=banner&utm_campaign=LTU_Vilnius_Web_COURIER_LOCAL_VNO12&utm_content=FILLQ */}
                     <div className="col-span-2">
                         Su Wolt neįtikėtinai lengvai atrasi ir gausi tai, ko nori. Puikių restoranų meniu, maisto prekės ir viskas, ko tau gali prireikti bus pristatyta - greitai, patikimai ir už prieinamą kainą.
-                        Taip pat: matomi banner'iai turėtų link'inti į:https://wolt.com/lt/apply-as-courier?utm_source=Affiliate&utm_medium=banner&utm_campaign=LTU_Vilnius_Web_COURIER_LOCAL_VNO12&utm_content=FILLQ
                     </div>
-                    <div>
-                        <img src={ZyroLogo} alt="logo" />
+                    <div className="flex justify-center items-center">
+                        <img src={ZyroLogo} alt="logo" className="h-20 cursor-pointer" onClick={() => {window.open("https://zyro.com/lt", "_blank")}}/>
                     </div>
                     <div className="col-span-2">
                         Susikurk svetainę ar el. parduotuvę su Zyro lengvai ir greitai! Gauk iki 86% nuolaidą, 3 nemokamus mėnesius ir domeną dovanų su kodu FILLQ. (Nuoroda)
                     </div>
 
-                    <div>
+                    <div className="flex justify-center items-center">
                         <img src={SurfsharkLogo} alt="logo" />
                     </div>
                     <div className="col-span-2">
                         Po visų duomenų nesaugumo skandalų būtų nuodėmė naršyti internete be VPN. Gera žinia ta, kad Surfshark paruošė jums labai gerą deal‘ą. Naudok kodą FILLQ(https://surfshark.deals/FILLQ) ir gauk 83% nuolaidą bei nemokamai net keturis papildomus mėnesius.
                     </div>
-                    <div>
+                    <div className="flex justify-center items-center">
                         <img src={Tuta} alt="logo" />
                     </div>
                     <div className="col-span-2">
