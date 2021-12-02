1c91db3d4d96e43a01eea84064771088fe160f70
Armandas Barkauskas <52072845+barkisjr@users.noreply.github.com>
2021-12-02 10:53:33 +0200

    WIP

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,16 +11,16 @@ const Layout = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col  justify-center items-center  ">
             <main className="flex-grow container mx-auto px-4 sm:px-6">
-                <img src={TopoCentrasLogo} className="m-auto mt-12" alt="topo centras" />
+                <img src={TopoCentrasLogo} onClick={() => { window.open("https://www.topocentras.lt/", "_blank") }}  className="cursor-pointer m-auto mt-12" alt="topo centras" />
                 {children}
                 <div className="flex mb-12">
-                    <img src={ViperLogo} className="m-auto mt-12 max-h-10" alt="topo centras" />
-                    <img src={SurfsharkLogo} className="m-auto mt-12 max-h-10" alt="topo centras" />
-                    <img src={Tuta} className="m-auto mt-12 max-h-10" alt="topo centras" />
-                    <img src={ZyroLogo} className="m-auto mt-12 max-h-10" alt="topo centras" />
-                    <img src={Wolt} className="m-auto mt-12 max-h-10" alt="topo centras" />
-                    <img src={Razer} className="m-auto mt-12 max-h-16" alt="topo centras" />
-                    <img src={CandyPop} className="m-auto mt-12 max-h-14" alt="topo centras" />
+                    <img src={ViperLogo} onClick={() => { window.open("https://viper.patriotmemory.com/", "_blank") }}  className="m-auto mt-12 max-h-10 cursor-pointer" alt="topo centras" />
+                    <img src={SurfsharkLogo} onClick={() => { window.open("https://surfshark.deals/FILLQ", "_blank") }} className="m-auto mt-12 max-h-10 cursor-pointer" alt="topo centras" />
+                    <img src={Tuta} onClick={() => { window.open("https://tuta.lt/", "_blank") }} className="m-auto mt-12 max-h-10 cursor-pointer" alt="topo centras" />
+                    <img src={ZyroLogo} onClick={() => { window.open("https://zyro.com/lt/special/fillq")}} className="m-auto mt-12 max-h-10 cursor-pointer" alt="topo centras" />
+                    <img src={Wolt} onClick={() => { window.open("https://wolt.com/lt/apply-as-courier?utm_source=Affiliate&utm_medium=banner&utm_campaign=LTU_Vilnius_Web_COURIER_LOCAL_VNO12&utm_content=FILLQ", "_blank") }} className="m-auto mt-12 max-h-10  cursor-pointer" alt="topo centras" />
+                    <img src={Razer} onClick={() => { window.open("https://www.topocentras.lt/razer", "_blank") }}  className="m-auto mt-12 max-h-16 cursor-pointer" alt="topo centras" />
+                    <img src={CandyPop} onClick={() => { window.open("https://www.candypop.lt", "_blank") }}  className="m-auto mt-12 max-h-14 cursor-pointer" alt="topo centras" />
                 </div>
 
             </main>
