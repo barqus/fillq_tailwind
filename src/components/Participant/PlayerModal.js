d2a3c49e41fba5f7c1d1bd36fac945e141c836aa
Armandas Barkauskas <52072845+barkisjr@users.noreply.github.com>
2021-12-04 09:03:42 +0200

    WIP

--- src/components/Participant/PlayerModal.js
+++ src/components/Participant/PlayerModal.js
@@ -50,7 +50,7 @@ const PlayerModal = ({ setShowModal, participant }) => {
                 <div
                     className="flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     style={{ background: "linear-gradient(120deg,#722f818a 0%, rgba(3, 71, 57, 0.7) 100%)" }} >
-                    <div className="relative w-8/12 my-12 mx-auto max-w-8xl">
+                    <div className="relative sm:w-12/12 md:w-8/12 my-12 mx-auto max-w-8xl">
                         <div className="w-6xl border-2 border-opacity-20 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-900 outline-none focus:outline-none" ref={wrapperRef}>
                             <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t bg-gradient-to-r from-purple-800 to-green-500 ">
                                 <h3 className="text-3xl font-semibold text-white">
@@ -73,7 +73,7 @@ const PlayerModal = ({ setShowModal, participant }) => {
                                 <div class="grid md:grid-cols-4 sm:grid-cols-1 gap-4">
                                     <div className="">
 
-                                        <img style={imageLoaded ? {} : { display: 'none' }} className="border-2 border-purple-400 border-opacity-60 filter drop-shadow-2xl object-cover mt-5 w-60 h-60 inline rounded-lg"
+                                        <img style={imageLoaded ? {} : { display: 'none' }} className=" self-center border-2 border-purple-400 border-opacity-60 filter drop-shadow-2xl object-cover mt-5 inline rounded-lg"
                                             src={participant.description} width="500px" alt="dalyvis" loading="lazy" onLoad={() => setImageLoaded(true)} />
                                         <p className="mt-4 mb-2 text-purple-400">{participant.name}</p>
                                         <p className="my-2 text-purple-400">{participant.surname}</p>
